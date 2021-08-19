6c83fca2699b2d8eb1dfb8a5672468c7c713e6e4
Jean Tessier <jean.tessier@gusto.com>
2021-08-18 19:29:19 -0700

    Match consumer to that for wisper-sidekiq
    I made the subscriber for 10 seconds to simulate long-ish jobs.  The
    request returns immediately while Kafka manages the running subscriber.

--- app/subscribers/echo_subscriber.rb
+++ app/subscribers/echo_subscriber.rb
@@ -4,5 +4,12 @@ class EchoSubscriber
       [#{self}] echo event:
       [#{self}]     name: #{name} [#{name.class.name}]
     MSG
+
+    actual_sleep = sleep(10)
+
+    Rails.logger.info <<~MSG
+      [#{self}] Slept #{actual_sleep} secs.
+      [#{self}] Done.
+    MSG
   end
 end
